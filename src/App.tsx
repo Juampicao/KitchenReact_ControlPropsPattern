6259fd124302045b877dacc3478b9c0e9e27f05a
juampicao <juanpiparada@gmail.com>
2023-01-27 11:26:46 -0300

    img

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { ShoppingPage } from './components/kitchen-proyect/pages/ShoppingPage';
 import "./index.css";
 function App() {
   return (
+    <>
+
     <div>
       <ShoppingPage/>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
